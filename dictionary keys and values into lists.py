51ab526844e45883554511439e7cc47925b8e8bb
FriedBallerina <FriedBallerina@users.noreply.github.com>
2019-04-07 16:45:13 -0700

    Exercises from Chapter 5

---
+++ dictionary keys and values into lists.py
@@ -0,0 +1,5 @@
+spam = {'color': 'red', 'age': 42}
+characteristics = list(spam.keys())
+respective_values = list(spam.values())
+
+print('The characteristics in dictionary are: ' + str(characteristics) + ' and the respective values are: ' + str(respective_values))
---
+++ dictionary1.py
@@ -0,0 +1,3 @@
+myDog = {'size' : 'medium', 'color' : 'multicolored', 'disposition' : 'loyal'}
+
+print('My dog is ' + myDog['size'] + ', it is ' + myDog['color'] + ' and it is extremely ' + myDog['disposition'] + '. I love my dog!')
